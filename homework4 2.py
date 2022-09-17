97df998695c21fd67606ee8c83ac330f9b5f0a81
Anatoly Finchenko <finchenko777@gmail.com>
2022-09-17 08:30:08 +0300

    *two2*

--- homework4 2.py
+++ homework4 2.py
@@ -3,30 +3,14 @@
 
 number = int(input("Задайте число : "))
 list = []
-
-while (number >= 2):
-    if number % 2 == 0:
-        number /= 2
-        list.append(2)
-    if number % 3 == 0:
-        number /= 3
-        list.append(3)
-    if number % 5 == 0:
-        number /= 5
-        list.append(5)
-    if number % 7 == 0:
-        number /= 7
-        list.append(7)
-    if number % 11 == 0:
-        number /= 11
-        list.append(11)
-    if number % 13 == 0:
-        number /= 13
-        list.append(13)
-    if number % 17 == 0:
-        number /= 17
-        list.append(17)
-    if number % 19 == 0:
-        number /= 19
-        list.append(19)
+while (number > 2 or number > 1):
+    i = 2
+    number1 = number
+    print("11111")
+    while (number == number1):
+        if number % i == 0:
+            number /= i
+            print(i)
+            list.append(i)
+        i += 1
 print(list)
